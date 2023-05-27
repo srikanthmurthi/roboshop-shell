928b6b22f0e87a241c75854e9eec649839e27bb9
murthisrikanth <sreekanthnani222@gmail.com>
2023-05-28 05:28:43 +0530

    roboshop nginx script

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,5 @@
 yum install nginx -y
 
-systemctl enable nginx
-systemctl start nginx
-
-
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
