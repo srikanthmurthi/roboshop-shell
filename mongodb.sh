2c64275ccbb53147d7f1e0aa0a0259c6f7c01a89
murthisrikanth <sreekanthnani222@gmail.com>
2023-05-28 18:06:15 +0530

    mongo db script

--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,9 @@
 echo -e "\e[33mCopy MongoDB Repo file \e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
+cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 echo -e "\e[33mInstalling MongoDB Server\e[0m"
-yum install mongodb-org -y
-##Modify the config file
+yum install mongodb-org -y &>>/tmp/roboshop.log
+echo -e "\e[33mUpdate MongoDB listen address\e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo -e "\e[33mStart MongoDB Service\e[0m"
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
